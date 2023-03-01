9136194bbd278b2d3e1879c23dc62d2533f12e00
OmasAdmin <omax.f3@gmail.com>
2023-03-01 14:07:25 +0000

    my second commit

---
+++ profileMgt.java
@@ -0,0 +1,13 @@
+Welcome to our Software solution
+We have a dedicated team of coders that ensures maximum satifaction with all your software needs
+we take pride in creating the following solution for your business needs
+
+
+Java
+Python
+PhpMysql
+OracleDB
+PHP webscript etc
+
+Join our team of Engineers
+
